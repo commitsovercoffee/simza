10eec24ad8631d9af6a3e3394a0eb1d789565edf
commitsovercoffee <commitsovercoffee@gmail.com>
2024-01-06 22:19:32 +0530

    adds card selection mechanism

--- src/lib/components/Deck.svelte
+++ src/lib/components/Deck.svelte
@@ -1,28 +1,45 @@
 <script>
+	const cardStyle =
+		'absolute rounded p-2 m-2 w-12 h-18 border border-slate-600 bg-gray-200 shadow transition-transform duration-200 ease-in delay-50 ';
+
 	let cards = Array.from({ length: 78 }, (_, i) => i + 1);
-	let currentCard = -1;
+	let selected = Array(3).fill(-1);
+	let chances = 3;
 
 	function shuffleArray() {
 		for (let i = cards.length - 1; i > 0; i--) {
-			// Generate a random index lower than the current element's index
 			const j = Math.floor(Math.random() * (i + 1));
-
-			// Swap the elements at indices i and j
 			[cards[i], cards[j]] = [cards[j], cards[i]];
-
-			currentCard = j;
 		}
-		currentCard = -1;
 		console.log(cards);
 		return cards;
+	}
+
+	function handleCardClick(index) {
+		if (selected[0] == -1) {
+			selected[0] = index;
+		} else if (selected[1] == -1) {
+			selected[1] = index;
+		} else if (selected[2] == -1) {
+			selected[2] = index;
+		}
+		console.log(selected);
+		chances--;
 	}
 </script>
 
 {#each cards as card, index}
-	<div
-		style="left:{index * 6.5}px; top:{currentCard == index ? '10px' : '0px'}  "
-		class="absolute rounded p-2 m-2 w-12 h-18 bg-gray-200 odd:bg-gray-400 hover:translate-y-8 transition-transform duration-200 ease-in delay-50"
+	<button
+		on:click={() => {
+			chances > 0 && handleCardClick(index);
+		}}
+		style="left:{index * 6.5}px; top:20px"
+		class={selected.includes(index)
+			? cardStyle + ' bg-red-600 translate-y-32'
+			: chances > 0
+				? cardStyle + ' hover:translate-y-8 '
+				: cardStyle}
 	>
 		<p>{card}</p>
-	</div>
+	</button>
 {/each}
